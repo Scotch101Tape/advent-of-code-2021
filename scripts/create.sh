395d7fe373fe5fbbc1c90c09d720357c9b760d19
Scotch101Tape <66388250+Scotch101Tape@users.noreply.github.com>
2021-11-27 16:13:44 +0000

    Added scripts; Initial program

---
+++ scripts/create.sh
@@ -0,0 +1,8 @@
+cd ./solutions
+mkdir day-$1
+
+cd ./day-$1
+touch input.txt
+touch solution.txt
+
+echo 'print("Hello World")' > main.py
---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+cd ./solutions/day-$1
+
+python main.py
+
+echo ./solutions/day-$1/solution.txt contains the solution

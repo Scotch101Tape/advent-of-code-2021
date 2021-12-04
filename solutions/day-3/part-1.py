964bbf8ae0e6d777b8aac48d78447b1c1697d4eb
Scotch101Tape <scotch101tape@gmail.com>
2021-12-03 20:47:48 -0600

    Finish Day 3

--- solutions/day-3/part-1.py
+++ solutions/day-3/part-1.py
@@ -1,1 +1,23 @@
-input = open("input.txt", "r")
+input = open('input.txt', 'r')
+
+# 12 long
+count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+def list_to_int(l):
+    num = 0
+    for i in range(12):
+        num += int(l[i]) * pow(2, 11 - i)
+    return num
+
+for line in input.readlines():
+    for i in range(12):
+        digit = line[i]
+        if digit == "1":
+            count[i] += 1
+        elif digit == "0":
+            count[i] -= 1
+    
+gamma = list(map(lambda x: min(max(x, 0), 1), count.copy()))
+elipson = list(map(lambda x: min(max(-x, 0), 1), count.copy()))
+
+print(list_to_int(gamma) * list_to_int(elipson))
--- solutions/day-3/part-2.py
+++ solutions/day-3/part-2.py
@@ -1,1 +1,49 @@
-input = open("input.txt", "r")
+from os import error
+
+input = open('input.txt', 'r')
+
+def list_to_int(l):
+    num = 0
+    for i in range(12):
+        num += int(l[i]) * pow(2, 11 - i)
+    return num
+
+lines = input.readlines()
+
+# Oxygen
+oxygen_lines = lines.copy()
+
+for i in range(12):
+    count = 0
+    for line in oxygen_lines:
+        if line[i] == "1":
+            count += 1
+        elif line[i] == "0":
+            count -= 1
+
+    chosen_bit = "1" if count >= 0 else "0"
+    oxygen_lines = list(filter(lambda x: x[i] == chosen_bit, oxygen_lines))
+
+    if len(oxygen_lines) == 1:
+        oxygen = list_to_int(oxygen_lines[0])
+        break
+
+# CO2
+co2_lines = lines.copy()
+
+for i in range(12):
+    count = 0
+    for line in co2_lines:
+        if line[i] == "1":
+            count += 1
+        elif line[i] == "0":
+            count -= 1
+
+    chosen_bit = "1" if count < 0 else "0"
+    co2_lines = list(filter(lambda x: x[i] == chosen_bit, co2_lines))
+
+    if len(co2_lines) == 1:
+        co2 = list_to_int(co2_lines[0])
+        break
+
+print(oxygen * co2)

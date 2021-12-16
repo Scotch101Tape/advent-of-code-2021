f67819825c46bca9fb9433f81a99837f3eadaaea
Scotch101Tape <scotch101tape@gmail.com>
2021-12-15 20:57:58 -0600

    Fix Day Names

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,7 +1,14 @@
-cd ./solutions
-mkdir day-$1
+if [ ${#1} -gt 1 ]
+then
+    day_num=$1
+else
+    day_num=0$1
+fi
 
-cd ./day-$1
+cd ./solutions
+mkdir day-$day_num
+
+cd ./day-$day_num
 touch input.txt
 
 echo 'input = open("input.txt", "r")' > part-1.py
